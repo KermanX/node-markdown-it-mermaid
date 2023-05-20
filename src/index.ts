988d65d9d8f81e04fe90befab9fe8eae728a373b
_Kerman <kermanx@qq.com>
2023-05-20 06:21:53 +0000

    update

--- src/index.ts
+++ src/index.ts
@@ -1,52 +1,43 @@
-import nodeMermaid from 'node-mermaid';
-import type { PluginSimple } from 'markdown-it'
+//import nodeMermaid from "node-mermaid";
+import mermaid, { MermaidConfig } from "mermaid";
+import type { PluginSimple } from "markdown-it";
 
-const mermaidChart = (code: string) => {
-  try {
-    mermaid.parse(code)
-    return `<div class="mermaid">${code}</div>`
-  } catch ({ str, hash }) {
-    return `<pre>${str}</pre>`
-  }
-}
+export default function NodeMermaidPlugin(
+  mermaidConfig: Omit<MermaidConfig, "startOnLoad"> = {}
+): PluginSimple {
+  const mermaidChart = async (code: string) => {
+    try {
+      return (await mermaid.render("graphDiv", code)).svg;
+    } catch (e) {
+      return `<pre>${e}</pre>`;
+    }
+  };
 
-const MermaidPlugin: PluginSimple = (md) => {
-  // md.mermaid = mermaid
-  mermaid.loadPreferences = (preferenceStore) => {
-    let mermaidTheme = preferenceStore.get('mermaid-theme') ?? 'default'
-    let ganttAxisFormat = preferenceStore.get('gantt-axis-format') ?? '%Y-%m-%d'
+  return (md) => {
     mermaid.initialize({
-      theme: mermaidTheme,
-      gantt: {
-        axisFormatter: [
-          [ganttAxisFormat, (d) => {
-            return d.getDay() === 1
-          }]
-        ]
+      ...mermaidConfig,
+      startOnLoad: false,
+    });
+
+    const temp = md.renderer.rules.fence?.bind(md.renderer.rules);
+    if (!temp) {
+      throw new Error("md.renderer.rules.fence is null/undefined !");
+    }
+    md.renderer.rules.fence = (tokens, idx, options, env, slf) => {
+      const token = tokens[idx];
+      const code = token.content.trim();
+      if (token.info === "mermaid") {
+        return mermaidChart(code);
       }
-    })
-    return {
-      'mermaid-theme': mermaidTheme,
-      'gantt-axis-format': ganttAxisFormat
-    }
-  }
-
-  const temp = md.renderer.rules.fence?.bind(md.renderer.rules)
-  if (!temp) {
-    throw new Error('md.renderer.rules.fence is null/undefined !')
-  }
-  md.renderer.rules.fence = (tokens, idx, options, env, slf) => {
-    const token = tokens[idx]
-    const code = token.content.trim()
-    if (token.info === 'mermaid') {
-      return mermaidChart(code)
-    }
-    const firstLine = code.split(/\n/)[0].trim()
-    if (firstLine === 'gantt' || firstLine === 'sequenceDiagram' || firstLine.match(/^graph (?:TB|BT|RL|LR|TD);?$/)) {
-      return mermaidChart(code)
-    }
-    return temp(tokens, idx, options, env, slf)
-  }
+      const firstLine = code.split(/\n/)[0].trim();
+      if (
+        firstLine === "gantt" ||
+        firstLine === "sequenceDiagram" ||
+        firstLine.match(/^graph (?:TB|BT|RL|LR|TD);?$/)
+      ) {
+        return mermaidChart(code);
+      }
+      return temp(tokens, idx, options, env, slf);
+    };
+  };
 }
-
-export default MermaidPlugin
